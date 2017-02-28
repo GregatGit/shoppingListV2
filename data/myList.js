c640b3da21854177ab1e224688965e835db69750
Greg Duncan <gregdd@outlook.com>
2017-02-28 15:05:15 +1000

    removed underscore from items

--- data/myList.js
+++ data/myList.js
@@ -7,14 +7,14 @@ let list1 = [
     ['tomatoes', 'produce', 'Fruit_Market'],
     ['milk', 'fresh', 'Coles', true],
     ['cheese', 'fresh', 'Aldi'],
-    ['chicken_legs', 'fresh', 'Aldi'],
-    ['rye_bread', 'fresh', 'Coles'],
-    ['dish_soap', 'fresh', 'Aldi'],
+    ['chicken legs', 'fresh', 'Aldi'],
+    ['rye bread', 'fresh', 'Coles'],
+    ['dish soap', 'fresh', 'Aldi'],
     ['tissues', 'fresh', 'Aldi'],
-    ['face_wash', 'fresh', 'Aldi'],
+    ['face wash', 'fresh', 'Aldi'],
     ['coffee', 'fresh', 'Coles'],
     ['tea', 'fresh', 'Aldi'],
-    ['soft_drink', 'fresh', 'Coles']
+    ['soft drink', 'fresh', 'Coles']
 ];
 
 let list2 = [
@@ -60,13 +60,13 @@ let myNewList = [];
 //     {"name": "oranges", "category": "produce", "store": "Fruit_Market", "weekly": false},
 //     {"name": "tomatoes", "category": "produce", "store": "Fruit_Market", "weekly": true},
 //     {"name": "milk", "category": "fresh", "store": "Aldi", "weekly": true},
-//     {"name": "chicken_legs", "category": "fresh", "store": "Aldi", "weekly": false},
-//     {"name": "rye_bread", "category": "fresh", "store": "Coles", "weekly": false},
-//     {"name": "dish_soap", "category": "cleaning", "store": "Aldi", "weekly": false},
+//     {"name": "chicken legs", "category": "fresh", "store": "Aldi", "weekly": false},
+//     {"name": "rye bread", "category": "fresh", "store": "Coles", "weekly": false},
+//     {"name": "dish soap", "category": "cleaning", "store": "Aldi", "weekly": false},
 //     {"name": "tissues", "category": "health", "store": "Aldi", "weekly": false},
 //     {"name": "face wash", "category": "health", "store": "Aldi", "weekly": false},
 //     {"name": "coffee", "category": "beverages", "store": "Coles", "weekly": false},
 //     {"name": "tea", "category": "beverages", "store": "Aldi", "weekly": false},
-//     {"name": "soft_drink", "category": "beverages", "store": "Aldi", "weekly": false}
+//     {"name": "soft drink", "category": "beverages", "store": "Aldi", "weekly": false}
 // ];
 
